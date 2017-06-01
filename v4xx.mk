53a95e4b08d1f876ed286cead8ea1eeced511d01
David Lin <dtwlin@google.com>
2017-06-01 12:58:37 -0700

    v4xx-common: implement vibrator 1.1 HAL
    Implement the new perform 1.1 function for supporting tick effect.
    
    Bug: 62176703
    Test: VtsHalVibratorV1_1TargetTest
    Change-Id: Icbd50c2e7d05fd520aeda4511ba95151dde2a5ed

--- v4xx.mk
+++ v4xx.mk
@@ -231,7 +231,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.v4xx
+    android.hardware.vibrator@1.1-service.v4xx
 
 # Wifi
 PRODUCT_PACKAGES += \
