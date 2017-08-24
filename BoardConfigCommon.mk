c4240d48ad7b72a2d5e22793de5e0a38c1639e8d
Simon Shields <simon@lineageos.org>
2017-08-24 13:50:28 +1000

    v4xx-common: build HIDL HALs
    Change-Id: Iae90afd048da90f41b5b6acd1766a27400c7cf49

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,6 +88,7 @@ TARGET_USE_COMPAT_GRALLOC_PERFORM := true
 EXTENDED_FONT_FOOTPRINT := true
 
 # HIDL
+DEVICE_MANIFEST_FILE := device/lge/v4xx-common/configs/manifest.xml
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 # Lights
