45e2bdeafd17a0a8f3ab593ae1f5fdc06ab961df
Ethan Chen <intervigil@gmail.com>
2017-09-16 23:19:53 -0700

    [WIP]v4xx-common: Add vibrator HAL in place of CMHW implementation TODO: Rework HAL for v4xx virbration paths
    Change-Id: I19d4f916d807301ec30b882bfdeaf76715a041c7

--- v4xx.mk
+++ v4xx.mk
@@ -272,7 +272,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.v4xx
 
 # Wifi
 PRODUCT_PACKAGES += \
