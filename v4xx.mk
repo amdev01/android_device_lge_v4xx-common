1eb68ef56e41c8b53a839ac4724baabb2bc00a4e
Ethan Chen <intervigil@gmail.com>
2017-09-17 02:27:36 -0700

    v4xx-common: Convert lights HAL to native binderized HAL
    Simplify HAL for v4xx
    
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

--- v4xx.mk
+++ v4xx.mk
@@ -157,8 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8226
+    android.hardware.light@2.0-service.v4xx
 
 # Media
 PRODUCT_COPY_FILES += \
