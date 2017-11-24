d84d6465ea89f24933ab987a453fe25343784c95
Simon Shields <simon@lineageos.org>
2017-11-24 05:17:00 +0200

    v4xx-common: Move audio config to vendor
    Change-Id: I70fdf7df5813bba193e634666e18390a282da581

--- v4xx.mk
+++ v4xx.mk
@@ -91,7 +91,8 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
