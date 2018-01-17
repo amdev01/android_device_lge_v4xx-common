83535c82b6b1554b25646b1760369bf01f5de6ce
KhabarlakKonstantin <habarlack@gmail.com>
2018-01-17 19:04:12 +0200

    v4xx-common: Remove deprecated audiod service
    Change-Id: I07e8e4b8ef90c9102e1a3182cce5bc7599125868

--- v4xx.mk
+++ v4xx.mk
@@ -78,7 +78,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    audiod \
     android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8226 \
