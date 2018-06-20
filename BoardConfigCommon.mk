8358d238d3cee380f959dab4e7124e70b82f01ce
Arnau <arnaullv@gmail.com>
2018-06-20 22:04:00 +0200

    v4xx-common: Remove TARGET_PROVIDES_LIBLIGHT
    We are using binderized HAL
    
    Change-Id: I0be9a5302b1bd43ebfd8e0d88349b8115cc278e6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,9 +91,6 @@ EXTENDED_FONT_FOOTPRINT := true
 DEVICE_MANIFEST_FILE := device/lge/v4xx-common/configs/manifest.xml
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Offmode Charging
 BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
 BOARD_CHARGING_CMDLINE_VALUE := "chargerlogo"
