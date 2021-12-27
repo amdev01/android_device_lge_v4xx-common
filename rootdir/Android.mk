eb9881571e39baebad2c53c5116a2df9870ff72b
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2021-12-27 18:34:43 +0000

    Revert: [updated] init: Create wifi directory into /data/vendor path
    As per Android-O architecture, move vendor specific file into
    /data/vendor path instead of /data/misc. To address this
    create the wifi directory into /data/vendor path.
    
    CRs-Fixed: 2047794
    Change-Id: I6835fb70e1777e23d0b30aa9e08de9a746638c50

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -8,14 +8,78 @@ LOCAL_SRC_FILES         := etc/fstab.v4xx
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
-# Init rc scripts
+# Init scripts
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.v4xx.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.v4xx.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.lge.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.lge.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.bt_vendor.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.bt_vendor.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.class_core.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.class_core.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw-sensor.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw-sensor.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.usb.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.usb.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zetaw.usb.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.zetaw.usb.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -26,72 +90,3 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/ueventd.v4xx.rc
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
-
-# Init vendor rcs
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.zetaw.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.lge.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.lge.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.bt_vendor.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.zetaw.bt_vendor.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.usb.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.zetaw.usb.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-# Init sh scripts
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.class_core.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := bin/init.zetaw.class_core.sh
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw-sensor.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := bin/init.zetaw-sensor.sh
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := bin/init.zetaw.sh
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zetaw.usb.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := bin/init.zetaw.usb.sh
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
