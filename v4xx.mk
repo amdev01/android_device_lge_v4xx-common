fcb719ca0efd73f710dec6799023d2f876ab1f63
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2021-12-28 13:33:45 +0000

    v4xx: Update some props

--- v4xx.mk
+++ v4xx.mk
@@ -221,7 +221,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    power.msm8226
 
 # Qualcomm
 PRODUCT_PACKAGES += \
