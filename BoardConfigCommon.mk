eefa649f45051d50b2b79eeef69d8e0dacfc6f7d
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2022-07-16 14:35:12 +0100

    camera: Update camera parameters shim path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,7 +142,7 @@ TARGET_USES_LEGACY_ADB_INTERFACE := true
 # Shims
 TARGET_LD_SHIM_LIBS := /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
         /system/vendor/lib/libOpenCL.so|libboringssl-compat.so \
-        /system/vendor/lib/libcamera_client.so|libcamera_parameters_shim.so \
+        /system/lib/libcamera_client.so|libcamera_parameters_shim.so \
         /system/lib/hw/camera.vendor.msm8226.so|libshim_atomic.so \
         /system/bin/mpdecision|libshim_atomic.so
 
