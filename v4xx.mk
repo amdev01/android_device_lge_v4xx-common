2faa8cff8b82cfe3f69e4f6551168cd1fa259ffb
amdev01 <am.dev.mail01@gmail.com>
2026-07-28 20:44:19 +0100

    Remove timekeep service and related configurations from recovery and init files; - fixes WiFi  autoconnect bug

--- v4xx.mk
+++ v4xx.mk
@@ -50,11 +50,6 @@ PRODUCT_COPY_FILES += \
 -include $(LOCAL_PATH)/system_prop.mk
 PRODUCT_CHARACTERISTICS := tablet
 
-# Sony timekeep
-PRODUCT_PACKAGES += \
-    timekeep \
-    TimeKeep
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
